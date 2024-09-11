78ca7a50f748fb5ec7de15b807b1bf19cca6ca96
FaisalAlharbidev <164782595+FaisalAlharbidev@users.noreply.github.com>
2024-09-11 14:48:51 +0300

    Add files via upload

---
+++ domLab2.js
@@ -0,0 +1,15 @@
+
+let cube = document.getElementById('box');
+let container = document.body; // أو أي عنصر آخر يحتوي على المربع
+
+function moveCube() {
+  let newLeft = Math.random() * container.clientWidth;
+  let newTop = Math.random() * container.clientHeight;
+  let transformString = "translate(" + newLeft + "px, " + newTop + "px) scale(" + (Math.random() * 2 + 0.5) + ")";
+  let backgroundColorString = "rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", " + Math.random() * 255 + ")";
+
+  cube.style.transform = transformString;
+  cube.style.backgroundColor = backgroundColorString;
+}
+
+setInterval(moveCube, 2000);
